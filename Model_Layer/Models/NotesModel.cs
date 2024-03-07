2dcc8e20de64f56bdc40ca7a6d13ec025ff515a5
Hemant1420 <153729212+Hemant1420@users.noreply.github.com>
2024-03-07 17:28:52 +0530

    Update NotesModel.cs
    Removed Isdeleted and Isarchived properties

--- Model_Layer/Models/NotesModel.cs
+++ Model_Layer/Models/NotesModel.cs
@@ -13,12 +13,10 @@ namespace Model_Layer.Models
         public string Title { get; set; }
 
         public string Description { get; set; }
+        
         public string Colour { get; set; } = "";
 
-        public bool IsArchived { get; set; } = false;
-
-        public bool IsDeleted { get; set; } = false;    
-
+        
 
     }
 }
